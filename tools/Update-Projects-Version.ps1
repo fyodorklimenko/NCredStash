c25452e136aab3f0671b5e826b7dbf0dfd1dcb6d
Serhiy Kokhan <kokhan_s@ukr.net>
2017-02-03 14:58:14 +0200

    Change project.json versions.

--- tools/Update-Projects-Version.ps1
+++ tools/Update-Projects-Version.ps1
@@ -2,7 +2,7 @@ Get-ChildItem -Path $ENV:APPVEYOR_BUILD_FOLDER -Recurse –File -Filter project.js
     $jsonFile = Get-Content $_.FullName -raw | ConvertFrom-Json
     if($jsonFile.version)
     {
-        $jsonFile.version = "3.0." + $ENV:APPVEYOR_BUILD_NUMBER + "-0"
+        $jsonFile.version = "3.0.1-build" + $ENV:APPVEYOR_BUILD_NUMBER
         $jsonFile | ConvertTo-Json -Depth 999 | Out-File $_.FullName
     }
 }
