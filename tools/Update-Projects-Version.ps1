4341f203e23fc44247ccdf6506d3c9bcd07f5f98
Serhiy Kokhan <KSERKO@beijerelectronics.com>
2017-06-13 18:53:38 +0300

    Remove unused items.
    Modify appveyor.yml

--- tools/Update-Projects-Version.ps1
+++
@@ -1,8 +0,0 @@
-Get-ChildItem -Path $ENV:APPVEYOR_BUILD_FOLDER -Recurse –File -Filter project.json | foreach {
-    $jsonFile = Get-Content $_.FullName -raw | ConvertFrom-Json
-    if($jsonFile.version)
-    {
-        $jsonFile.version = "3.1." + $ENV:APPVEYOR_BUILD_NUMBER + "-*"
-        $jsonFile | ConvertTo-Json -Depth 999 | Out-File $_.FullName
-    }
-}
